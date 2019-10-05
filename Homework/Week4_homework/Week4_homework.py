c847abbaea10c92155897838e0c059a6eb237d62
笔记 <32284098+kuangzipeng@users.noreply.github.com>
2019-10-05 17:36:26 +0800

    Add files via upload

---
+++ Homework/Week4_homework/Week4_homework.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+#week4作业
+
+#计算面积
+from geometry.area.circle import area_circle
+area_circle()
+from geometry.area.rectangle import area_rectangle
+area_rectangle()
+from geometry.area.square import area_square
+area_square()
+from geometry.area.triangles import area_triangles
+area_triangles()
+
+#计算体积
+from geometry.volume.cube import vol_cube
+vol_cube()
+from geometry.volume.cuboid import vol_cuboid
+vol_cuboid()
+from geometry.volume.cylinder import vol_cylinder
+vol_cylinder()
+from geometry.volume.sphere import vol_sphere
+vol_sphere()
+
+#计算表面积
+from geometry.surface_area.cube import sur_cube
+sur_cube()
+from geometry.surface_area.cuboid import sur_cuboid
+sur_cuboid()
+from geometry.surface_area.cylinder import sur_cylinder
+sur_cylinder()
+from geometry.surface_area.sphere import sur_sphere
+sur_sphere()
+
+
+# In[ ]:
+
+
+
+    
+
+
+# In[ ]:
+
+
+
+
